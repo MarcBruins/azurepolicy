37b59bc047758919158b6890a533cafe55f1f59e
Marc Bruins <marc@marcbruins.nl>
2021-12-19 11:14:33 +0100

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,1 +1,5 @@
+./scripts/deploy-policyDef.ps1 -FolderPath ./policy-definitions/resource-diagnostics-settings/ -recurse -managementGroupName Spokes-Non-Prod -silent
+
+Write-Output "Next up the intitivatie...."
+
 ./scripts/deploy-policySetDef.ps1 -definitionFile ./initiative-definitions/resource-diagnostics-settings/log-analytics/azurepolicyset-la.json -managementGroupName Spokes-Non-Prod -PolicyLocations @{policyLocationResourceId1 = '/providers/Microsoft.Management/managementGroups/Spokes-Non-Prod'}
